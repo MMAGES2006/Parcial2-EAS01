89d8e4164f2004eaf6fc4bc1799089ac363d7fde
MateoMagdaleno <22170@portalcedi.edu.mx>
2024-03-04 13:53:01 -0600

    2

--- jugadores.cpp
+++ jugadores.cpp
@@ -12,7 +12,7 @@ public:
     int vida = 0;
     int ataque = 0;
     string nombre;
-    string *ptrj;
+    Jugador *ptrj;
     /*
         Agrega propiedades para guardar la vida(número entero), ataque(número entero) y nombre del jugador.
         Agrega una propiedad para guardar el jugador que lo atacó por última vez.
@@ -21,7 +21,7 @@ public:
     Jugador();
     Jugador(int hp, int atk, string name);
     /* agrega parámetros para asignar hp, ataque y nombre */
-    void atacar(); /* agregar parámetros para atacar a otro jugador*/
+    void atacar(Jugador to_ataque); /* agregar parámetros para atacar a otro jugador*/
 };
 
 int main()
@@ -67,17 +67,18 @@ Jugador::Jugador(int hp, int atk, string name) /* Agrega parámetros para asigna
     */
 }
 
-void Jugador::atacar() /* Agregar parámetros para atacar a otro Jugador */
+void Jugador::atacar(Jugador to_ataque) /* Agregar parámetros para atacar a otro Jugador */
 {
-
-    /*
-        El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
-        Existe un 20% de probabilidad de que el ataque falle y no haga daño.
-        Se debe mostrar lo que pasó en cada ataque:
-        nombre1 atacó a nombre2 e hizo 5 de daño, vida restante de nombre2 = 95
-        nombre2 atacó a nombre1 pero fallo, vida restante de nombre1 = 100
-        El jugador que atacó debe quedar guardar en la propiedad atacadoPor del jugador atacado.
-    */
+    cout << "el jugador: " << ptrj << "ataco a:" <<
+        /*
+            El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
+            Existe un 20% de probabilidad de que el ataque falle y no haga daño.
+            Se debe mostrar lo que pasó en cada ataque:
+            nombre1 atacó a nombre2 e hizo 5 de daño, vida restante de nombre2 = 95
+            nombre2 atacó a nombre1 pero fallo, vida restante de nombre1 = 100
+            El jugador que atacó debe quedar guardar en la propiedad atacadoPor del jugador atacado.
+        */
+        int x = rand() % 100;
     /*
     Nota:
         int x = rand() & 1000; asigna un valor aleatorio entre 0 y 999 a x;
