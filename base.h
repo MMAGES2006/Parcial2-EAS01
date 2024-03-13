e0f155b479396bfe3cf821f1382ffcff61c5ba5f
MateoMagdaleno <22170@portalcedi.edu.mx>
2024-03-13 13:22:54 -0600

    4

--- base.h
+++ base.h
@@ -1,8 +1,19 @@
-#ifdef base_h
+#ifndef base_h
 #define base_h
+
+#include "jugador.h"
+#include "enemigo.h"
 
 class Base
 {
-}
+public:
+    int vida;
+    int ataque;
+    string nombre;
+    Base();
+    Base(int vida, int ataque, string nombre);
+
+    void atacarJugador();
+};
 
 #endif
